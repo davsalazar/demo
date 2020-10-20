97b8192f4b94d006a5554cf49b51b81fd6d87bd9
David Salazar <davidsalazar@davids-mbp.home>
2020-10-20 14:24:26 -0700

    Added http security rule

--- management/main.tf
+++ management/main.tf
@@ -61,6 +61,18 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix       = "*"
     destination_address_prefix  = "*"
   }
+  
+  security_rule {
+    name                      = "HTTP"
+    priority                  = 120
+    direction                 = "Inbound"
+    access                    = "Allow"
+    protocol                  = "Tcp"
+    source_port_range         = "*"
+    destination_port_range    = "80"
+    source_address_prefix     = "*"
+    destination_address_prefix= "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "main" {
